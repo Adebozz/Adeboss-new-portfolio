8226adf4e18050f3069fefa4f2e43c559847d355
Adebozz <ademolaadeigbe3@gmail.com>
2025-05-29 11:18:45 +0100

    `updated the _app.js file `

---
+++ components/Navbar.js
@@ -0,0 +1,29 @@
+import { Box, Flex, IconButton, useColorMode, useColorModeValue, Spacer } from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import Link from 'next/link';
+
+export default function Navbar() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const bg = useColorModeValue('gray.100', 'gray.900');
+
+  return (
+    <Box bg={bg} px={4} position="fixed" width="100%" zIndex={999}>
+      <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+        <Box fontWeight="bold" fontSize="xl">My Portfolio</Box>
+        <Spacer />
+        <Flex alignItems={'center'}>
+          <Link href="#about" className="px-3">About</Link>
+          <Link href="#projects" className="px-3">Projects</Link>
+          <Link href="#contact" className="px-3">Contact</Link>
+          <IconButton
+            ml={4}
+            icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+            onClick={toggleColorMode}
+            variant="ghost"
+            aria-label="Toggle dark mode"
+          />
+        </Flex>
+      </Flex>
+    </Box>
+  );
+}
